1f60eb4ecff581a71de1ba3f68a4cf69d9639c81
Csatacsirke <battlechicken74@gmail.com>
2022-12-13 23:58:44 +0100

    13

--- 13/program13.py
+++ 13/program13.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import cmp_to_key
 import re 
 import math
 
@@ -35,6 +36,13 @@ def is_right_order(packet1, packet2):
 
 	raise
 
+def cmp_right_order(a, b):
+	result = is_right_order(a, b)
+	if result is None:
+		return 0
+	if result is True:
+		return -1
+	return 1
 
 def solve(lines):
 
@@ -42,12 +50,18 @@ def solve(lines):
 
 	pairs = []
 
+	packets = []
+
 	while line1 := next(iterator, None):
 		line2 = next(iterator)
 		_ = next(iterator, None)
 
 		packet1 = eval(line1)
 		packet2 = eval(line2)
+		
+		packets.append(packet1)
+		packets.append(packet2)
+
 		pairs.append((packet1, packet2))
 
 	final_result = 0
@@ -59,7 +73,28 @@ def solve(lines):
 		
 		continue
 
-	print(final_result)
+	divider1 = [[2]]
+	divider2 = [[6]]
+
+	packets.append(divider1)
+	packets.append(divider2)
+
+	#print(final_result)
+	sorted_packets = sorted(packets, key=cmp_to_key(cmp_right_order))
+	#packets.sort(key=cmp_right_order)
+	
+	divider1_index = None
+	divider2_index = None
+
+	for i in range(0, len(sorted_packets)):
+		if cmp_right_order(sorted_packets[i], divider1) == 0:
+			divider1_index = i + 1
+		if cmp_right_order(sorted_packets[i], divider2) == 0:
+			divider2_index = i + 1
+
+
+	print(divider1_index * divider2_index)
+
 
 	return 
 
